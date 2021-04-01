a6c6024da892f32fd8db581a3ecbb7572636582f
Maycon <mayconfelipe164@gmail.com>
2021-04-01 12:18:28 -0300

    User entity and resource

---
+++ src/main/java/com/example/projetoweb/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.example.projetoweb.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.projetoweb.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+
+public class UserResource {
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maycon", "maycon@gmail.com", "99291", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
