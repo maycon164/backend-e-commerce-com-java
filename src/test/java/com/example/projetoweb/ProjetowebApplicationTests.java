d921a37a05858adf8327773e0ff22e66248c3b34
Maycon <mayconfelipe164@gmail.com>
2021-04-01 11:35:20 -0300

    Project created

---
+++ src/test/java/com/example/projetoweb/ProjetowebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetowebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
