a10b0139e65cac8375b7c4c5017fda64339ccb45
Maycon <mayconfelipe164@gmail.com>
2021-04-02 09:44:36 -0300

    OrderStatus enum

--- src/main/java/com/example/projetoweb/entities/Order.java
+++ src/main/java/com/example/projetoweb/entities/Order.java
@@ -11,6 +11,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.example.projetoweb.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
@@ -26,6 +27,8 @@ public class Order implements Serializable {
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant instant;
 
+	private Integer orderStatus;
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
@@ -34,10 +37,11 @@ public class Order implements Serializable {
 
 	}
 
-	public Order(Long id, Instant instant, User client) {
+	public Order(Long id, Instant instant, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.instant = instant;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -88,6 +92,17 @@ public class Order implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+
 	}
 
 }
