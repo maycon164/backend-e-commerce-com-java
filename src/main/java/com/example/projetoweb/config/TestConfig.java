aadbb4f24eefc25b3cb8165248afb9abcbc09b39
Maycon <mayconfelipe164@gmail.com>
2021-04-02 17:16:03 -0300

    Product

--- src/main/java/com/example/projetoweb/config/TestConfig.java
+++ src/main/java/com/example/projetoweb/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.example.projetoweb.entities.Category;
 import com.example.projetoweb.entities.Order;
+import com.example.projetoweb.entities.Product;
 import com.example.projetoweb.entities.User;
 import com.example.projetoweb.entities.enums.OrderStatus;
 import com.example.projetoweb.repositories.CategoryRepository;
 import com.example.projetoweb.repositories.OrderRepository;
+import com.example.projetoweb.repositories.ProductRepository;
 import com.example.projetoweb.repositories.UserRepository;
 
 @Configuration
@@ -26,6 +28,8 @@ public class TestConfig implements CommandLineRunner {
 	OrderRepository OrderRepository;
 	@Autowired
 	CategoryRepository categoryRepository;
+	@Autowired
+	ProductRepository productRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -33,6 +37,13 @@ public class TestConfig implements CommandLineRunner {
 		Category c2 = new Category(null, "Books");
 		Category c3 = new Category(null, "Software");
 		categoryRepository.saveAll(Arrays.asList(c1, c2, c3));
+
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
 		User u1 = new User(null, "mayk", "mayk@gmail.com", "1234", "3333");
 		User u2 = new User(null, "felipe", "felipe@gmail.com", "99999", "1204");
---
+++ src/main/java/com/example/projetoweb/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.example.projetoweb.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.projetoweb.entities.Product;
+import com.example.projetoweb.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
