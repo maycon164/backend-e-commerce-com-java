b1e16c63ce7ef22ea95f2ba605e024a6cbe37b57
Maycon <mayconfelipe164@gmail.com>
2021-04-02 09:04:30 -0300

    Order, Instant, ISO 8601 part2

--- src/main/java/com/example/projetoweb/config/TestConfig.java
+++ src/main/java/com/example/projetoweb/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.example.projetoweb.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,22 +8,31 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.example.projetoweb.entities.Order;
 import com.example.projetoweb.entities.User;
+import com.example.projetoweb.repositories.OrderRepository;
 import com.example.projetoweb.repositories.UserRepository;
 
 @Configuration
 @Profile("test")
-public class TestConfig implements CommandLineRunner{
+public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	UserRepository userRepository;
-	
+	@Autowired
+	OrderRepository OrderRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
 		User u1 = new User(null, "mayk", "mayk@gmail.com", "1234", "3333");
 		User u2 = new User(null, "felipe", "felipe@gmail.com", "99999", "1204");
 		User u3 = new User(null, "jean", "jean@gmail.com", "323311", "9103");
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));
+		
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		OrderRepository.saveAll(Arrays.asList(o1, o2, o3));
 	}
 
 }
